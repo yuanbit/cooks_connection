f9e024d81397c9d2c10ac76ddc7302e6b6cbb50c
Bithiah Yuan <bithiah@Bithiahs-MacBook-Air-2.local>
2025-02-26 09:06:43 +0100

    updated vite for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/cooks_connection/", // 👈 Add your GitHub repo name here
   server: {
     host: "::",
     port: 8080,
